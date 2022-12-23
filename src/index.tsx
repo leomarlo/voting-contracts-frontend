70875e2473c895b1c95fab253ff47453aeca767a
Leonhard Horstmeyer <leo.h@hotmail.de>
2022-12-23 23:57:15 +0100

    minor change in index.tsx --> no more Test importing

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from "react";
 
 import { createRoot } from "react-dom/client";
 import { Dapp } from "./Dapp";
-import { AppTest } from "./components/Test2";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.css";
