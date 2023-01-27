c42c030c4eafbba4e020e185c9c1dfcaccec3c7e
Leonhard Horstmeyer <leo.h@hotmail.de>
2023-01-27 14:38:32 +0100

    added some text to the About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -10,17 +10,35 @@ interface AboutArgs {
 const AboutComp: React.FC<AboutArgs> = ({ detailsHandling }: AboutArgs) => {
   let thisPageInfo: PageInfo = pageInfo[Pages.About]
 
-  const Details = (
-    <>
-      <div style={{ backgroundColor: "cornflowerblue" }} className="card">
-        Details
-      </div>
-    </>
-  )
+
   return (
     <div key={thisPageInfo.key}>
-      <h3>{thisPageInfo.title}</h3>
-      <button onClick={() => detailsHandling.detailsSetter(Details)}>Click me</button>
+      {/* <h3>{thisPageInfo.title}</h3> */}
+      <p>
+        We make on-chain voting configurable, modularizable, pluggable and secure.
+        The key idea behind configurable voting contracts is to outsource the voting functionality
+        in much the same way as one would outsource a token contract. This comes with many advantages.
+        First of all the separation of the voting logic from the logic of a contract that integrates voting (e.g. a DAO) allows
+        for them to be updated or replugged, respectively. Second, there is no need to hardcode the same type of voting functionality
+        into thousands of DAOs yet to come. Third, the changing demands on voting functionality can be accounted for by adding ever new types
+        of voting contracts, such as token-weighted voting, delegate voting, quadratic voting etc.
+      </p>
+      <p>
+        This platform aims to fulfil three purposes. First we propose a standard for configurable voting contracts which is exposed <span>here</span>,
+        second the platform provides an easy way to build both voting contracts that obey the standard aswell as <span>interfacing</span> them inside of your target contract, i.e. integration.
+        Lastly there is a <span>playground</span> that serves the purpose of familiarizing yourself with the idea of voting contracts.
+      </p>
+      Please find th following information on this page:
+      <ol>
+        <li><span>Voting Contracts</span></li>
+        <li><span>Integration</span></li>
+        <li><span>Registry</span></li>
+        <li><span>Playground</span></li>
+      </ol>
+
+
+      The core concept of <b>Configurable Voting Contracts</b> is to outsource voting to external
+
     </div>
   )
 }
